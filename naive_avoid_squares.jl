aa397249eceb3a78ffcc925958b95ec13342397a
Clinton Nolan <clinton.nolan@gmail.com>
2020-07-16 01:04:07 -0500

    This is slightly faster

--- naive_avoid_squares.jl
+++ naive_avoid_squares.jl
@@ -26,18 +26,18 @@ function create_square_indexes(n)
     error("The board must have at least one square")
   end
   if n == 1
-    return Array{NTuple{4,Int64},1}()
+    return Array{NTuple{8,Int64},1}()
   end
 
-  square_indexes = Array{NTuple{4,NTuple{2,Int64}},1}()
+  square_indexes = Array{NTuple{8,Int64},1}()
   # Generate "flat" square bitmasks
   for square_size::UInt64=1:n-1
     for y=1:n-square_size
       for x=1:n-square_size
-        push!(square_indexes,((x,y),
-                              (x+square_size,y),
-                              (x,y+square_size),
-                              (x+square_size,y+square_size)))
+        push!(square_indexes,(x,y,
+                              x+square_size,y,
+                              x,y+square_size,
+                              x+square_size,y+square_size))
       end
     end
   end
@@ -46,10 +46,10 @@ function create_square_indexes(n)
   for square_size::UInt64=1:n÷2
     for y=1:n-square_size*2
       for x=square_size+1:n-square_size
-        push!(square_indexes,((x,y),
-                              (x+square_size,y+square_size),
-                              (x-square_size,y+square_size),
-                              (x,y+2*square_size)))
+        push!(square_indexes,(x,y,
+                              x+square_size,y+square_size,
+                              x-square_size,y+square_size,
+                              x,y+2*square_size))
       end
     end
   end
@@ -57,25 +57,12 @@ function create_square_indexes(n)
   return square_indexes
 end
 
-#=
-#Display square_indexes
-square_indexes = create_square_indexes(N)
-for square_index in square_indexes
-  board = zeros(Bool, N, N)
-  for index in square_index
-    board[index[1],index[2]] = true
-  end
-  displaysquarebits(board)
-  println()
-end
-=#
-
 function has_square(board, square_indexes)
   for square_index in square_indexes
-    first_corner::Bool = board[square_index[1][1],square_index[1][2]]
-    if board[square_index[2][1],square_index[2][2]] == first_corner &&
-       board[square_index[3][1],square_index[3][2]] == first_corner &&
-       board[square_index[4][1],square_index[4][2]] == first_corner
+    first_corner::Bool = board[square_index[1],square_index[2]]
+    if board[square_index[3],square_index[4]] == first_corner &&
+       board[square_index[5],square_index[6]] == first_corner &&
+       board[square_index[7],square_index[8]] == first_corner
        return true
      end
   end
